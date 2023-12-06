5a0c0b531225619729d686e1cd901736cf2ff233
Yuu <youcef.messaoudene@zenika.com>
2023-12-06 15:58:05 +0100

    feat(api): Add types for Badge et Service

--- api/types/Service.ts
+++ api/types/Service.ts
@@ -2,9 +2,15 @@ export interface Service {
   id: number;
   title: string;
   status: string;
+  details: string;
   content: string;
 }
 
-export type ServiceFromApi = Omit<Service, 'id' | 'status'> & {
+export type ServiceFromApi = Omit<Service, 'id' | 'status' | 'details'> & {
   link: string;
+};
+
+export type StatusItem = {
+  status: string;
+  url: string;
 };
